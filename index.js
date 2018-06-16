691e53ee6963cdd42c6a7f39f90333c1acc30d09
Stephanie <koykoy1897@gmail.com>
2018-06-16 07:01:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,4 +22,8 @@ function sayHiToGrandma (string){
   else if(string.toUpperCase() === string){
     return "YES INDEED!"
   }
+
+  else if(string === "I love you, Grandma."){
+    return "I love you, too."
+  }
 }
